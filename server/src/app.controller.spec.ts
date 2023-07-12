613a1f9927adf467ae8d2247db184a4dab234a94
miravicson <victorughonu@gmail.com>
2023-07-13 00:17:35 +0100

    updated test

--- server/src/app.controller.spec.ts
+++ server/src/app.controller.spec.ts
@@ -1,29 +1,29 @@
 import { createMock } from '@golevelup/ts-jest';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseSeed } from './seeder/database.seed';
 
 describe('AppController', () => {
   let appController: AppController;
-  const configService = new Map<string, string>([['APP_NAME', 'test']]);
+  const configService = { get: jest.fn() };
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService, ConfigService, { provide: DatabaseSeed, useValue: createMock<DatabaseSeed>() }],
+      providers: [AppService, { provide: ConfigService, useValue: configService }],
     })
-      .overrideProvider(ConfigService)
-      .useValue(configService)
+      .useMocker(createMock)
       .compile();
 
     appController = app.get<AppController>(AppController);
   });
 
   describe('root', () => {
-    const appName = 'test';
+    const appName = 'Test';
+    configService.get.mockReturnValueOnce(appName);
+
     const message = `Welcome to ${appName}`;
     it('should return a welcome message"', () => {
       expect(appController.root()).toEqual({ message });
